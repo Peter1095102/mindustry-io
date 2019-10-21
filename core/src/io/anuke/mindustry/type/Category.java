fffd4a469e5e0eccc80b2d1d575fa2d6dcbdfa27
Wiki Updater <admin@catbox.wtf>
2019-10-22 01:17:57 +0200

    published

---
+++ core/src/io/anuke/mindustry/type/Category.java
@@ -0,0 +1,30 @@
+package io.anuke.mindustry.type;
+
+public enum Category{
+    /** Offensive turrets. */
+    turret,
+    /** Blocks that produce raw resources, such as drills. */
+    production,
+    /** Blocks that move items around. */
+    distribution,
+    /** Blocks that move liquids around. */
+    liquid,
+    /** Blocks that generate or transport power. */
+    power,
+    /** Walls and other defensive structures. */
+    defense,
+    /** Blocks that craft things. */
+    crafting,
+    /** Uranium-related things. */
+    uranium,
+    /** Blocks that create units. */
+    units,
+    /** Things that upgrade the player such as mech pads. */
+    upgrade,
+    /** Things for storage or passive effects. */
+    effect,
+    /** Statues & misc*/
+    misc;
+
+    public static final Category[] all = values();
+}
